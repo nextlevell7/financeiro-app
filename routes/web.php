169b42b6d81c698eab7bf9780eee55f858af2461
Matheus <matheuspvh1@gmail.com>
2026-04-29 20:04:22 -0400

    adicionar login breeze

--- routes/web.php
+++ routes/web.php
@@ -1,9 +1,20 @@
 <?php
 
-use App\Http\Controllers\TransacaoController;
+use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', [TransacaoController::class, 'index'])->name('financeiro.index');
-Route::post('/transacoes', [TransacaoController::class, 'store'])->name('transacoes.store');
-Route::delete('/transacoes/{transacao}', [TransacaoController::class, 'destroy'])->name('transacoes.destroy');
-Route::get('/relatorio', [TransacaoController::class, 'relatorio'])->name('financeiro.relatorio');
+Route::get('/', function () {
+    return view('welcome');
+});
+
+Route::get('/dashboard', function () {
+    return view('dashboard');
+})->middleware(['auth', 'verified'])->name('dashboard');
+
+Route::middleware('auth')->group(function () {
+    Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
+    Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
+    Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
+});
+
+require __DIR__.'/auth.php';
