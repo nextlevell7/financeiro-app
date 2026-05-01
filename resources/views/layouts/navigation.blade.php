77704ee8c99cd3cf9cfabc338dea1dbb806a913d
Matheus <matheuspvh1@gmail.com>
2026-05-01 11:01:55 -0400

    remove profile route error

--- resources/views/layouts/navigation.blade.php
+++ resources/views/layouts/navigation.blade.php
@@ -34,9 +34,9 @@
                     </x-slot>
 
                     <x-slot name="content">
-                        <x-dropdown-link :href="route('profile.edit')">
-                            {{ __('Profile') }}
-                        </x-dropdown-link>
+                        <x-dropdown-link :href="route('financeiro.index')">
+    Dashboard
+</x-dropdown-link>
 
                         <!-- Authentication -->
                         <form method="POST" action="{{ route('logout') }}">
